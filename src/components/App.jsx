dad9c44e47bbb461494dcfb609ff0a0f2b6e4f49
bhanujggandhi <gandhibhanuj@gmail.com>
2020-05-01 14:56:14 +0530

    Add Delete functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './Footer';
 import Note from './Note';
 import Header from './Header';
-import notes from '../notes';
+import CreateArea from './CreateArea';
 
 function App() {
+	const [ notes, setNotes ] = useState([]);
+
+	function addNote(newNote) {
+		setNotes((prevNotes) => {
+			return [ ...prevNotes, newNote ];
+		});
+	}
+
+	function deleteNote(id) {
+		setNotes((prevNotes) => {
+			return prevNotes.filter((noteItem, index) => {
+				return index !== id;
+			});
+		});
+	}
+
 	return (
 		<div>
 			<Header />
-			{notes.map((newNote) => <Note key={newNote.key} title={newNote.title} content={newNote.content} />)}
+			<CreateArea onAdd={addNote} />
+			{notes.map((noteItem, index) => {
+				return (
+					<Note
+						key={index}
+						id={index}
+						title={noteItem.title}
+						content={noteItem.content}
+						onDelete={deleteNote}
+					/>
+				);
+			})}
 			<Footer />
 		</div>
 	);
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ const year = new Date().getFullYear();
 function Footer() {
 	return (
 		<footer>
-			<p>Copyright © {year}</p>
+			<p>© {year} Bhanuj Gandhi. All rights reserved.</p>
 		</footer>
 	);
 }
